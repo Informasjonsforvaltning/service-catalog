04e1b0ef3b971a25143146079cbbe5d53cd49b36
Pooria Mehregan <pooriamehregan@gmail.com>
2023-10-30 13:41:57 +0100

    feat: get public service by id

--- src/main/kotlin/no/digdir/service_catalog/service/PublicServiceService.kt
+++ src/main/kotlin/no/digdir/service_catalog/service/PublicServiceService.kt
@@ -1,10 +1,17 @@
 package no.digdir.service_catalog.service
 
+import no.digdir.service_catalog.model.PublicService
 import no.digdir.service_catalog.mongodb.PublicServiceRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class PublicServiceService(private val publicServiceRepository: PublicServiceRepository) {
     fun findPublicServicesByCatalogId(catalogId: String) =
         publicServiceRepository.getByCatalogId(catalogId)
+
+    fun findPublicServiceById(id: String, catalogId: String) =
+        publicServiceRepository
+            .findByIdOrNull(id)
+            ?.takeIf { it.catalogId == catalogId }
 }
--- src/test/kotlin/no/digdir/service_catalog/integration/PublicServices.kt
+++ src/test/kotlin/no/digdir/service_catalog/integration/PublicServices.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import no.digdir.service_catalog.model.PublicService
 import no.digdir.service_catalog.utils.ApiTestContext
 import no.digdir.service_catalog.utils.PUBLIC_SERVICES
+import no.digdir.service_catalog.utils.PUBLIC_SERVICE_1
 import no.digdir.service_catalog.utils.apiAuthorizedRequest
 import no.digdir.service_catalog.utils.jwt.Access
 import no.digdir.service_catalog.utils.jwt.JwtToken
@@ -27,7 +28,12 @@ class PublicServices: ApiTestContext() {
 
     @Test
     fun `able to get all public services`() {
-        val response = apiAuthorizedRequest("/catalogs/910244132/public-services", port, null, JwtToken(Access.ORG_READ).toString(), "GET")
+        val response = apiAuthorizedRequest(
+            "/catalogs/910244132/public-services",
+            port,
+            null,
+            JwtToken(Access.ORG_READ).toString(),
+            "GET")
         Assertions.assertEquals(HttpStatus.OK.value(), response["status"])
 
         val result: List<PublicService> = mapper.readValue(response["body"] as String)
@@ -36,13 +42,47 @@ class PublicServices: ApiTestContext() {
 
     @Test
     fun `unauthorized when missing token`() {
-        val response = apiAuthorizedRequest("/catalogs/910244132/public-services", port, null, null, "GET")
+        val response = apiAuthorizedRequest(
+            "/catalogs/910244132/public-services",
+            port,
+            null,
+            null,
+            "GET")
         Assertions.assertEquals(HttpStatus.UNAUTHORIZED.value(), response["status"])
     }
 
     @Test
     fun `forbidden when authorized for other catalog`() {
-        val response = apiAuthorizedRequest("/catalogs/910244132/public-services", port, null, JwtToken(Access.WRONG_ORG_READ).toString(), "GET")
+        val response = apiAuthorizedRequest(
+            "/catalogs/910244132/public-services",
+            port,
+            null,
+            JwtToken(Access.WRONG_ORG_READ).toString(),
+            "GET")
         Assertions.assertEquals(HttpStatus.FORBIDDEN.value(), response["status"])
+    }
+
+    @Test
+    fun `able to get public service by id`() {
+        val response = apiAuthorizedRequest(
+            "/catalogs/910244132/public-services/1",
+            port,
+            null,
+            JwtToken(Access.ORG_READ).toString(),
+            "GET")
+        Assertions.assertEquals(HttpStatus.OK.value(), response["status"])
+        val result: PublicService = mapper.readValue(response["body"] as String)
+        Assertions.assertEquals(PUBLIC_SERVICE_1, result)
+    }
+
+    @Test
+    fun `receive not found when public service is not found`() {
+        val response = apiAuthorizedRequest(
+            "/catalogs/910244132/public-services/1000",
+            port,
+            null,
+            JwtToken(Access.ORG_READ).toString(),
+            "GET")
+        Assertions.assertEquals(HttpStatus.NOT_FOUND.value(), response["status"])
     }
 }
