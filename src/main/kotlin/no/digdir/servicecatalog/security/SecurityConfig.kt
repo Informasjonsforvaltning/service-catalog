6ceadb5283e4d3c5dfc49df829910ec059516570
Hassan Alam <hassan@hotmail.no>
2026-01-12 16:39:17 +0100

    fix: update import for OAuth2ResourceServerProperties to new package location

--- src/main/kotlin/no/digdir/servicecatalog/security/SecurityConfig.kt
+++ src/main/kotlin/no/digdir/servicecatalog/security/SecurityConfig.kt
@@ -1,7 +1,7 @@
 package no.digdir.servicecatalog.security
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
+import org.springframework.boot.security.oauth2.server.resource.autoconfigure.OAuth2ResourceServerProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
