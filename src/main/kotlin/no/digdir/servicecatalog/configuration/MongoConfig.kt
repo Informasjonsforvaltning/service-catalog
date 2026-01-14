9e1fd6db3074e4276d9b1ee3a29bdfb3095a23c1
Hassan Alam <hassan@hotmail.no>
2026-01-14 12:10:51 +0100

    refactor: update MongoDB configuration properties to align with Spring Boot 4 standards

--- src/main/kotlin/no/digdir/servicecatalog/configuration/MongoConfig.kt
+++ src/main/kotlin/no/digdir/servicecatalog/configuration/MongoConfig.kt
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.MongoTemplate
 
 @Configuration
 open class MongoConfig(
-    @param:Value("\${spring.data.mongodb.database}")
+    @param:Value("\${spring.mongodb.database}")
     private val database: String
 ) {
 
