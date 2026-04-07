745d030b8808dfd267538eb9cfad3ea7124d25bd
Hassan Alam <hassan@hotmail.no>
2026-04-07 12:20:26 +0200

    refactor: checksum issue

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -9,3 +9,4 @@ CREATE TABLE services (
 CREATE INDEX idx_services_catalog_id ON services (catalog_id);
 CREATE INDEX idx_services_catalog_id_published ON services (catalog_id, published);
 CREATE INDEX idx_services_catalog_id_service_type ON services (catalog_id, service_type);
+
