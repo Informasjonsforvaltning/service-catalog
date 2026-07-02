68ec090f15482a9e9486d1cb0555d345a1a3a648
Hassan Alam <hassan@hotmail.no>
2026-07-02 09:32:12 +0200

    fix: pin isPartOf json property name

--- src/main/kotlin/no/digdir/servicecatalog/domain/Output.kt
+++ src/main/kotlin/no/digdir/servicecatalog/domain/Output.kt
@@ -2,6 +2,7 @@ package no.digdir.servicecatalog.domain
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -11,5 +12,7 @@ data class Output(
     val title: LocalizedStrings?,
     val description: LocalizedStrings?,
     val language: List<String>?,
+    @get:JsonProperty("isPartOf")
+    @param:JsonProperty("isPartOf")
     val isPartOf: List<String>?
 )
